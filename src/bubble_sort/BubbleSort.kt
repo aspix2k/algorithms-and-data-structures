209d1550efe5749a608b441806da6351aa79fd7c
aspix <10401349+aspix2k@users.noreply.github.com>
2019-12-19 13:13:05 +0300

    Added optimization for BubbleSort

--- src/bubble_sort/BubbleSort.kt
+++ src/bubble_sort/BubbleSort.kt
@@ -2,13 +2,16 @@ package bubble_sort
 
 fun sort(array: Array<Int>): Array<Int> {
     for (i in array.lastIndex - 1 downTo 0) {
+        var hasSwaps = false
         for (j in 0..i) {
             if (array[j] >= array[j + 1]) {
                 val temp = array[j]
                 array[j] = array[j + 1]
                 array[j + 1] = temp
+                hasSwaps = true
             }
         }
+        if (!hasSwaps) break
     }
     return array
 }
