918ac10c7573abe9959b4e9043f2d0c5d679516f
aspix <10401349+aspix2k@users.noreply.github.com>
2019-12-25 23:05:34 +0300

    Added selection sort

--- src/base/selection_sort/SelectionSort.kt
+++ src/base/selection_sort/SelectionSort.kt
@@ -2,11 +2,27 @@ package base.selection_sort
 
 /*
  * Description:
+ * Sort array using Selection Sort.
  *
+ * Input: unsorted array
+ * Output: sorted array
  *
  * Complexity: O(N²)
 */
 
-fun selectionSort() {
-
+fun selectionSort(array: Array<Int>): Array<Int> {
+    for (i in 0 until array.lastIndex) {
+        var min = array[i]
+        var minIndex = i
+        for (j in (i + 1)..array.lastIndex) {
+            if (array[j] < min) {
+                min = array[j]
+                minIndex = j
+            }
+        }
+        val temp = array[i]
+        array[i] = array[minIndex]
+        array[minIndex] = temp
+    }
+    return array
 }
--- src/base/selection_sort/SelectionSortTest.kt
+++ src/base/selection_sort/SelectionSortTest.kt
@@ -1,5 +1,23 @@
 package base.selection_sort
 
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
 class SelectionSortTest {
 
+    @Test
+    fun test1() {
+        assertArrayEquals(
+            arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+            selectionSort(arrayOf(8, 4, 3, 2, 9, 0, 1, 5, 7, 6))
+        )
+    }
+
+    @Test
+    fun test2() {
+        assertArrayEquals(
+            arrayOf(-66, -4, -1, 2, 3, 5, 7, 9, 100, 10000),
+            selectionSort(arrayOf(100, -1, 2, 3, -4, 5, -66, 7, 10000, 9))
+        )
+    }
 }
